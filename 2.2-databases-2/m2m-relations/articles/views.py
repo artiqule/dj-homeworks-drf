3eb8b8210bbc857576ddfbd7eecfbe209c86b2bb
Artem Moiseev <8868382@gmail.com>
2022-07-25 23:06:56 +0300

    fixed the relationship and realization the prefetch_related

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-
-from articles.models import Article
+from .models import Article, Scope, ArticleScope
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    context = {'articles': Article.objects.order_by('-published_at').all()}
 
     return render(request, template, context)
